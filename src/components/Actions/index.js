faafe0750a2f14afe887cbb18ba5034e4b2df099
Bruno Monteiro <bno_mts1@hotmail.com>
2022-11-02 10:09:25 -0300

    Primeiro commit

---
+++ src/components/Actions/index.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native'
+
+import { AntDesign } from '@expo/vector-icons'
+
+export default function Actions() {
+  return (
+    <ScrollView
+      style={styles.container}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+    >
+      <TouchableOpacity activeOpacity={0.5} style={styles.actionButton}>
+        <View style={styles.areaButton}>
+          <AntDesign name="addfolder" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Entradas</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity activeOpacity={0.5} style={styles.actionButton}>
+        <View style={styles.areaButton}>
+          <AntDesign name="tagso" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Compras</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity activeOpacity={0.5} style={styles.actionButton}>
+        <View style={styles.areaButton}>
+          <AntDesign name="creditcard" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Carteira</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity activeOpacity={0.5} style={styles.actionButton}>
+        <View style={styles.areaButton}>
+          <AntDesign name="barcode" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Boletos</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity activeOpacity={0.5} style={styles.actionButton}>
+        <View style={styles.areaButton}>
+          <AntDesign name="setting" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Conta</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingStart: 14,
+    paddingEnd: 14,
+    marginTop: 18,
+    marginBottom: 14,
+    maxHeight: 84,
+  },
+  actionButton: {
+    alignItems: 'center',
+    marginRight: 32,
+  },
+  areaButton: {
+    backgroundColor: '#ecf0f1',
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  labelButton: {
+    marginTop: 4,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+})
